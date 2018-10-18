de57028a2711a5f7e82b07c0801766d67e7e469f
curioushoainam <hoainamtruongute@gmail.com>
2018-10-18 21:57:20 +0700

    Event handle in ReactJS ES6

--- public/example2.js
+++ public/example2.js
@@ -2,16 +2,30 @@ class Note extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			tag: 'Donnerstag'
+			tag: 'Donnerstag',
+			val: 1
 		}
+
+		{/* this.addmore = this.addmore.bind(this); */}
+	}
+
+	addmore(){
+		this.state.val += 1;
+		this.setState(this.state);
 	}
 
 	render(){
 		return(
 			<div>
 				<p>Morgen ist {this.props.txt}</p>
-				<p>Heute is {this.state.tag}</p>
-				<button >Delete</button>
+				<p>Heute ist {this.state.tag}</p>
+				<p>Value is {this.state.val}</p>
+
+				{/* <button onClick={this.addmore} >Add more</button> */}
+				
+				<button onClick={this.addmore.bind(this)} >Add-more </button>
+				
+				
 			</div>
 		);
 	}
